60493054aef1e29be2b6f85442b653435bfc1fb1
James Butcher <jamesbutcher167@gmail.com>
2022-04-03 18:01:15 +0100

    Add shell command

--- wi/__main__.py
+++ wi/__main__.py
@@ -2,6 +2,9 @@ import wcore_py
 import click
 from pathlib import Path
 
+def get_next_input():
+    return "\n".join(iter(lambda: input("# ").strip(), ""))
+
 @click.group()
 def cli():
     pass
@@ -13,6 +16,13 @@ def run(path: str) -> None:
         content = f.read()
 
     wcore_py.eval(content)
+
+@cli.command()
+def shell() -> None:
+    state = wcore_py.State()
+
+    while (code_block := get_next_input()) != "exit":
+        state.apply(code_block)
 
 if __name__ == "__main__":
     cli()
