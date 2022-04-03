0d4da73605ac8b47cb745ae71282aa6479a2604c
James Butcher <jamesbutcher167@gmail.com>
2022-04-03 19:58:39 +0100

    Add better prefixing to shell

--- wi/__main__.py
+++ wi/__main__.py
@@ -2,20 +2,24 @@ import wcore_py
 import click
 from pathlib import Path
 
+
 def get_next_input():
-    return "\n".join(iter(lambda: input("# ").strip(), ""))
+    return f"{input('>>> ')}\n" + "\n".join(iter(lambda: input("... ").strip(), ""))
+
 
 @click.group()
 def cli():
     pass
 
+
 @cli.command()
-@click.argument('path')
+@click.argument("path")
 def run(path: str) -> None:
-    with open(str(Path('.')/path)) as f:
+    with open(str(Path(".") / path)) as f:
         content = f.read()
 
     wcore_py.eval(content)
+
 
 @cli.command()
 def shell() -> None:
@@ -23,6 +27,7 @@ def shell() -> None:
 
     while (code_block := get_next_input()) != "exit":
         state.apply(code_block)
+
 
 if __name__ == "__main__":
     cli()
